b44ba2e54b1acc3222edea0aca3e10f03f677ce4
AK Tanha <aktanha75@gmail.com>
2025-12-06 17:47:42 +0600

    feat: Update API base URLs to use localhost for auth and Vercel for blogs and comments.

--- frontend/src/redux/features/comments/commentApi.js
+++ frontend/src/redux/features/comments/commentApi.js
@@ -4,8 +4,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const commentApi = createApi({
     reducerPath: "commentApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5001/api/comments",
-        //baseUrl: "https://full-stack-blog-site-ontq.vercel.app/api/comments",
+        //baseUrl: "http://localhost:5001/api/comments",
+        baseUrl: "https://full-stack-blog-site-ontq.vercel.app/api/comments",
         credentials: "include"
     }),
     tagTypes: ['Comments'],
