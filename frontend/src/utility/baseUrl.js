2259dd5a7801520fb1a0ca1c28e6cfaac2ea0749
AK Tanha <aktanha75@gmail.com>
2026-04-27 21:38:19 +0600

    feat: update base URL to dynamically switch between local and production environments

--- frontend/src/utility/baseUrl.js
+++ frontend/src/utility/baseUrl.js
@@ -1,3 +1,5 @@
 export const getBaseUrl = () => {
-    return "https://full-stack-blog-site-ontq.vercel.app";
+    return window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+        ? "http://localhost:5001"
+        : "https://full-stack-blog-site-ontq.vercel.app";
 }
